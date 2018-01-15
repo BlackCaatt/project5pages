96c5f439da06ed0b8ac7f1c6199b90a2c3d6e56e
Ayneko <BlackCaatt@mail.ru>
2018-01-15 22:49:59 +0000

    1-2

--- folder/app.js
+++ folder/app.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var app = new express();
+var bodyParser = require('body-parser');
+
 
 app.get("/", function (req, res) {
     var form = '<!doctype html>\
@@ -12,17 +14,24 @@ app.get("/", function (req, res) {
         </head>\
         <body>\
         <form action="our_post" method="post">\
-        <input type="text" style="font-size:36px;"/>\
+        <input type="text" name="text" style="font-size:36px;"/>\
         <button style="font-size:36px;">Жми</button>\
         </form>\
         </body>\
         </html>'
     res.send(form)
+     
 
 })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.post("/our_post",function(req,res){
    res.send("<div style='font-size: 36px'>Ответ на пост запрос</div>")
  })
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
