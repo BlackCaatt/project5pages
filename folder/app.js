a0c690ace29cba71822e5185d0eae1619d006153
Ayneko <BlackCaatt@mail.ru>
2018-01-15 22:15:48 +0000

    with button and post

--- folder/app.js
+++ folder/app.js
@@ -1,10 +1,28 @@
 
 var express = require('express');
-var app = express();
+var app = new express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get("/", function (req, res) {
+    var form = '<!doctype html>\
+        <html lang="en">\
+        <head>\
+        <meta charset="UTF-8">\
+        <title>Document</title>\
+        <link rel="stylesheet" href="style.css"/>\
+        </head>\
+        <body>\
+        <form action="our_post" method="post">\
+        <input type="text" style="font-size:36px;"/>\
+        <button style="font-size:36px;">Жми</button>\
+        </form>\
+        </body>\
+        </html>'
+    res.send(form)
+
+})
+app.post("/our_post",function(req,res){
+   res.send("<div style='font-size: 36px'>Ответ на пост запрос</div>")
+ })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
